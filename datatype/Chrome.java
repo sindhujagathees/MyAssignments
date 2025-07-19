063fc743b22d105285edc0fe14de6bbd93042f65
sindhujagathees <155611392+sindhujagathees@users.noreply.github.com>
2025-07-19 18:22:56 +0530

    Add files via upload

---
+++ datatype/Chrome.java
@@ -0,0 +1,19 @@
+package datatype;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String name="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println(version);
+		System.out.println(name);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+
+	}
+
+}
